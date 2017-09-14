ee05b75edb08a3228320257994ce3144c6b3ef81
Unknown <adventurerka@mail.ru>
2017-09-14 18:32:03 +0300

    ДЗ2 часть 1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -6,22 +6,21 @@ public class Hello {
     hello("user");
     hello("people");
 
-    double len = 5;
-    System.out.println("Площадь квадрата со стороной "+len +" = "+area(len) );
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной "+s.l+" = "+s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами "+a+" и "+b+" = "+area(a,b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами "+r.a+" и "+r.b+" = "+r.area());
+
+    Point p = new Point(7, 8);
+    System.out.println("Расстояние до точки с координатами "+p.p1+" и "+p.p2+" = "+distance(p));
   }
 
   public static void hello(String somebody) {
 
     System.out.println("Hello "+ somebody);
   }
-  public static double area (double len){
-    return len*len;
-  }
-  public static double area (double a, double b){
-    return a*b;
+  public static double distance (Point p) {
+    return Math.sqrt(p.p1*p.p1+p.p2*p.p2);
   }
 }
