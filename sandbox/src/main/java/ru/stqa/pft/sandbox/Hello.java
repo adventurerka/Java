de5486bc038f2ed6fa6b5c87498836a107045d66
Unknown <adventurerka@mail.ru>
2017-09-18 14:44:48 +0300

    Вычисление расстояния между точками.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -12,15 +12,12 @@ public class Hello {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами "+r.a+" и "+r.b+" = "+r.area());
 
-    Point p = new Point(7, 8);
-    System.out.println("Расстояние до точки с координатами "+p.p1+" и "+p.p2+" = "+distance(p));
+    Point d = new Point (2,0,4,6);
+    System.out.println("Расстояние от точки с координатами "+d.p1+" и "+d.p2+" до точки с координатамии "+d.p3+" и "+d.p4+ " = "+d.distance());
   }
 
   public static void hello(String somebody) {
 
     System.out.println("Hello "+ somebody);
-  }
-  public static double distance (Point p) {
-    return Math.sqrt(p.p1*p.p1+p.p2*p.p2);
   }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,10 +4,18 @@ public class Point {
 
   public double p1;
   public double p2;
+  public double p3;
+  public double p4;
 
-
-  public Point (double p1, double p2){
+  public Point (double p1, double p2, double p3, double p4){
     this.p1 = p1;
     this.p2 = p2;
+    this.p3 = p3;
+    this.p4 = p4;
+  }
+
+  public double distance () {
+    return Math.sqrt(Math.pow((this.p1-this.p3),2)+Math.pow((this.p2-this.p4),2));
   }
   }
+
