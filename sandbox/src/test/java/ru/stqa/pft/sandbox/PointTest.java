be0415f72886cef32153ad2baa0317c037c265fb
Unknown <adventurerka@mail.ru>
2017-09-25 10:36:18 +0300

    Тесты для точки.

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+  @Test
+  public void testArea() {
+    Point p1 = new Point(0,0);
+    Point p2 = new Point(0,4);
+    Assert.assertEquals( p1.distance (p2),4.0);//true
+    Assert.assertEquals( p1.distance (p2),3.0);//false
+  }
+}
