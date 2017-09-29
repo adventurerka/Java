a3e17a5148c7ed35695fedd8b801d59e6ed2b725
Unknown <adventurerka@mail.ru>
2017-09-29 16:08:38 +0300

    Запуск в браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,21 +1,37 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    if (Objects.equals(browser, org.openqa.selenium.remote.BrowserType.FIREFOX)){
+      wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    } else if (Objects.equals(browser, org.openqa.selenium.remote.BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    } else if (Objects.equals(browser, org.openqa.selenium.remote.BrowserType.IE)){
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost:8080/addressbook/birthdays.php");
     sessionHelper = new SessionHelper(wd);
